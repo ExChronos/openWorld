4f5b3d8e59eda3f563b63beb662253aa63dafd54
Nathan <mega.nemirovskiy@gmail.com>
2023-12-11 10:54:19 +0300

    create new c++ file for learn in university

--- C_C++/new.cpp
+++ C_C++/new.cpp
@@ -1,26 +1,71 @@
 #include <iostream>
+#include <cstdlib> // Для использования функции rand()
+#include <ctime> // Для использования функции time()
 
-struct RatThing {
-    static int ratThingPower;
-    static void power_up_rat_thing(int nuclearIsotopes){
-        ratThingPower = ratThingPower + nuclearIsotopes;
-        const auto wasteHeat = ratThingPower * 20;
+using namespace std;
 
-        if(wasteHeat > 10000){
-            std::cout << "Warning ! Hot doggie\n";
+void generateRandomMatrix(int** matrix, int rows, int cols) {
+    srand(time(0)); // Устанавливаем seed для генерации псевдослучайных чисел
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = rand() % 100 - 50; // Генерируем случайное число от -50 до 49
         }
-        std::cout << "Rat-thing power: " << ratThingPower << std::endl;
     }
-};
+}
 
-int RatThing::ratThingPower = 200;
+void countPositiveElements(int** matrix, int rows, int cols) {
+    int* count = new int[cols]; // Создаем массив для хранения количества положительных элементов в каждом столбце
 
-int main(void)
-{
-    using namespace std;
+    for (int j = 0; j < cols; j++) {
+        count[j] = 0; // Инициализируем счетчик количества положительных элементов
 
-    RatThing::power_up_rat_thing(100);
-    RatThing::power_up_rat_thing(500);
-    
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][j] > 0) {
+                count[j]++;
+            }
+        }
+
+        cout << "Количество положительных элементов в столбце " << j + 1 << ": " << count[j] << endl;
+    }
+
+    delete[] count; // Освобождаем память
+}
+
+int main() {
+    int rows, cols;
+
+    cout << "Введите количество строк: ";
+    cin >> rows;
+
+    cout << "Введите количество столбцов: ";
+    cin >> cols;
+
+    int** A = new int*[rows]; // Создаем массив указателей на строки
+
+    for (int i = 0; i < rows; i++) {
+        A[i] = new int[cols]; // Создаем каждую строку массива
+    }
+
+    generateRandomMatrix(A, rows, cols);
+
+    cout << "Матрица: " << endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    countPositiveElements(A, rows, cols);
+
+    // Освобождаем память
+    for (int i = 0; i < rows; i++) {
+        delete[] A[i];
+    }
+    delete[] A;
+
     return 0;
 }

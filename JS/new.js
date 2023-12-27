09365e8568f71c044d79296fdbaa11b3abd251c3
Nathan <mega.nemirovskiy@gmail.com>
2023-12-27 11:09:34 +0300

    learned all basic syntaxis of JS and basic autotesting with 'Mocha' framework

--- JS/new.js
+++ JS/new.js
@@ -1,18 +1,41 @@
-'use strict';
-//let message = 'Hello'
-function ask(question, yes, no){
-    if(confirm(question))
-        yes();
-    else
-        no();
-}
+'use strict'
 
-function showOk(){
-    alert('You are confirm');
-}
+describe('pow', function(){
 
-function showCancel(){
-    alert("You canceled action");
-}
+    describe('Возводит x в степень 3', function(){
+        function makeTest(x){
+            let expexted = x * x * x;
+            it( `${x} в степени 3 будет ${expexted}`, function() {
+            assert.equal(pow(x,3), expexted);
+            });
+        }
 
-ask("Are you ready?", showOk, showCancel);
+        for(let i = 0; i <= 5; i++){
+            makeTest(i);
+        }
+    });
+
+    describe('Возводит x в степень n', function(){
+        let result = prompt('Введите степень для х', 5);
+
+        function makeTest(x){
+            let expexted = x**result;
+            it(`${x} в степени ${result} будет ${expexted}`, function(){
+                assert.equal(pow(x, result), expexted);
+            });
+        }
+
+        for(let i = 0; i < 5; i++){
+            makeTest(i);
+        }
+    });
+
+    it('Для отрицательных значений', function(){
+        assert.isNaN(pow(2, -1));
+    });
+
+    it('Для дробных n', function(){
+        assert.isNaN(pow(2, 0.5));
+    });
+
+});
